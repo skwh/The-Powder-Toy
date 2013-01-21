500f86f9f40015633cf15b33af2732483b017895
Evan Derby <somekidwithhtml@gmail.com>
2013-01-21 15:03:29 -0700

    Started Beryllium

---
+++ src/simulation/elements/BE.cpp
@@ -0,0 +1,11 @@
+#include "simulation/Elements.h"
+//#TPT-Directive ElementClass Element_BE PT_BE 175
+Element_BE::Element_BE() {
+
+}
+//#TPT-Directive ElementHeader Element_BE static int update(UPDATE_FUNC_ARGS)
+int Element_BE::update(UPDATE_FUNC_ARGS) {
+	return 0;
+}
+
+Element_BE::~Element_BE() {}
