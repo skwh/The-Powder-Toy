d2593a3e1da991b7fdc0924dbe5ac8048e0c5f08
Evan Derby <somekidwithhtml@gmail.com>
2013-01-21 15:03:07 -0700

    Added Water reaction

--- src/simulation/elements/MGO.cpp
+++ src/simulation/elements/MGO.cpp
@@ -47,6 +47,23 @@ Element_MGO::Element_MGO() {
 }
 //#TPT-Directive ElementHeader Element_MGO static int update(UPDATE_FUNC_ARGS)
 int Element_MGO::update(UPDATE_FUNC_ARGS) {
+	int r, rx, ry, rt;
+	parts[i].pavg[0] = x;
+	parts[i].pavg[1] = y;
+	for (rx=-2;rx<3;rx++) {
+		for (ry=-2;ry<3;ry++) {
+			if (x+rx>=0 && y+ry>=0 && x+rx<XRES && y+ry<YRES && pmap[y+ry][x+rx] &&
+                (pmap[y+ry][x+rx]&0xFF)!=PT_MG&&
+                (pmap[y+ry][x+rx]&0xFF)!=0xFF) {
+				r = sim->pmap[y+ry][x+rx];
+				rt = (r&0xFF);
+				if (rt == PT_WATR) {
+					sim->part_change_type(i,x,y,PT_MGOH2);
+					sim->kill_part(r>>8);
+				}
+			}
+		}
+	}
 	return 0;
 }
 
