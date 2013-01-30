d6f33ab0696e4a1b9a5b1cf92be150e3edef7741
Evan Derby <somekidwithhtml@gmail.com>
2013-01-30 16:07:14 -0700

    Generated BE

--- src/simulation/elements/BE.cpp
+++ src/simulation/elements/BE.cpp
@@ -1,11 +1,22 @@
-#include "simulation/Elements.h"
-//#TPT-Directive ElementClass Element_BE PT_BE 175
-Element_BE::Element_BE() {
 
-}
-//#TPT-Directive ElementHeader Element_BE static int update(UPDATE_FUNC_ARGS)
-int Element_BE::update(UPDATE_FUNC_ARGS) {
-	return 0;
-}
+        #include "simulation/Elements.h"
+        //#TPT-Directive ElementClass Element_BE PT_BE 175
+        Element_BE::Element_BE() {
+            Identifier = "DEFAULT_PT_BE";
+            Name = "Be";
+            ATMnumber = 4;
+            MenuVisible = 1;
+            MenuSection = SC_ELEMENTS;
+	    Enabled = 1;
 
-Element_BE::~Element_BE() {}
+            Description = "Beryllium";
+    
+            Update = &Element_BE::update;
+        }
+        //#TPT-Directive ElementHeader Element_BE static int update(UPDATE_FUNC_ARGS)
+        int Element_BE::update(UPDATE_FUNC_ARGS) {
+            return 0;
+        }
+
+        Element_BE::~Element_BE() {}
+        
